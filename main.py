c916198b16dd9f0c611762bdad2a57fcf15038c8
Sondos Mohamed <39069270+So47@users.noreply.github.com>
2024-04-06 08:22:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        stack = []
+        output = list(s)
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    output[i] = ''
+
+        for index in stack:
+            output[index] = ''
+        return ''.join(output)        
+        
